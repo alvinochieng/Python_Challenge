1dc6817c30962d86bfa681d486fd88bfc3112689
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-01-12 19:34:22 -0500

    Resubmit

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,57 +7,61 @@ output_path = os.path.join('PyBank.txt')
 
 #Variables that will be used throughout the code
 Date = []
-Profit_Losses = ()
-sum_months = 0
+Profit_Losses = []
+sum_months = 1
 net_prolo = 0
 avg_change = 0
-monthly_change = 0
+monthly_change = []
 tot_change = 0
+max_prolo = ["",-1000000000]
+min_prolo = ["",100000000000]
 
 #Reading the csv file
 with open (path, newline='') as csvfile:    
     csvreader = csv.reader(csvfile, delimiter=',')
-    
+
+#skips the header row    
     header = next(csvreader)
  
+    prev_row = next(csvreader)
+    net_prolo += int(prev_row[1])
+    Date.append(str(prev_row[0]))
+    Profit_Losses.append(int(prev_row[1]))
+    
 #creating a loop to collect data    
     for row in csvreader:
         
         Date.append(str(row[0]))
         Profit_Losses.append(int(row[1]))
-
+        
+#calculate monthly change
+        diff = int(row[1]) - int(prev_row[1])
+        monthly_change.append(diff)
+        prev_row = row
+        
 #formula for calculating total of the months                
         sum_months+= 1
         
 #formula for net total of profit/losses(prolo)
         net_prolo = net_prolo + (int(row[1]))
+        if diff < min_prolo[1]:
+            min_prolo[1] = diff
+            min_prolo[0] = row[0]
+        elif diff > max_prolo[1]:   
+            max_prolo[1] = diff
+            max_prolo[0]= row[0]     
         
 #formula for the average of changes in profit/losses
-        avg_change = net_prolo / sum_months
-        
-#Getting the greatest profit increase/ greatest loss decrease
-max_prolo = int(Profit_Losses[sum_months-1][1])-int(Profit_Losses[sum_months-2][1])
-min_prolo = int(Profit_Losses[sum_months-1][1])-int(Profit_Losses[sum_months-2][1])
-
-for i in range(sum_months,1,-1):
-    monthly_change = int(Profit_Losses[i-1][1]) - int(Profit_Losses[i-2][1])
-    
-    if monthly_change < min_prolo:
-        min_change = Profit_Losses[i-1][0]
-        min_prolo = monthly_change
-    elif monthly_change > max_prolo:   
-        max_prolo = monthly_change
-        max_change = Profit_Losses[i-1][0]
-        
-        prolo_change = tot_change + monthly_change
-        
-        print(min_change + max_change)
+avg_change = sum(monthly_change) / len(monthly_change)
    
-#Output the results to the terminal        
-        output_results = (
-            f"Total Months: {str(sum_months)}\n"
-            f"Net Total Profit/Losses: ${int(net_prolo)}\n"
-            f"Average Change: ${int(avg_change)}\n")
+#Output the results to the terminal
+#creating a dictionary output_results to prevent retyping when printing to the text file
+output_results = (
+    f"Total Months: {str(sum_months)}\n"
+    f"Net Total Profit/Losses: ${int(net_prolo)}\n"
+    f"Average Change: ${int(avg_change)}\n"
+    f"Greatest Increase in Profits: {max_prolo}\n"
+    f"Greatest Decrease in Losses: {min_prolo}")
 
 print(output_results)
 
